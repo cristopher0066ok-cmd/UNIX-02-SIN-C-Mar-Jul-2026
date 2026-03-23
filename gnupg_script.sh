e85e42b9d26980e7dd392a2348a63bd5b0ca0dbd
Cris10 <cristopher0066ok@gmail.com>
2026-03-23 15:47:11 +0000

    mensaje_encriptado

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -3,3 +3,7 @@ gpg --list-secret-keys --keyid-format=long  # comando para lista privada
 gpg --armor --export-secret-keys xxxxx # exporta la llaves secreta 
 gpg --import MICOMPA_llave_publica.asc # exporta la llave publica de un amigo
 gpg --list-keys #l lista de llaves incluida de mi amigo
+$ echo "este mensaje es secreto" > doc_no_cifrado.txt # mensaje para nuestro compa 
+cat doc-no_cifrado.txt # verificamos la creacion del contenido 
+gpg --output doc_cifrado.txt --encrypt --recipient xxxxxxxxxxxxxxxxxxxxxxx doc_no_cifrado.txt # cifrar el documento 
+gpg --decrypt cifrado_fran.txt # desifra el documento  
