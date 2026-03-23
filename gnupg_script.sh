a2f5de0924120d75a841b26f64f39c9cfbb25dc0
Cris10 <cristopher0066ok@gmail.com>
2026-03-23 15:10:52 +0000

    exportacion de llaves

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,5 @@
 # DFC4EF7DDAA317AC llave
 gpg --list-secret-keys --keyid-format=long  # comando para lista privada
 gpg --armor --export-secret-keys xxxxx # exporta la llaves secreta 
+gpg --import MICOMPA_llave_publica.asc # exporta la llave publica de un amigo
+gpg --list-keys #l lista de llaves incluida de mi amigo
