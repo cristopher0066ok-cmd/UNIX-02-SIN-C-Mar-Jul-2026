28544459eebff9cee297901cb2f7be72fa0492fa
Cris10 <cristopher0066ok@gmail.com>
2026-03-26 22:49:44 +0000

    gnupg_script.sh

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,18 +1,18 @@
 # DFC4EF7DDAA317AC llave
-gpg --list-secret-keys --keyid-format=long  # comando para lista privada
-gpg --armor --export-secret-keys xxxxx # exporta la llaves secreta 
-gpg --import MICOMPA_llave_publica.asc # exporta la llave publica de un amigo
-gpg --list-keys #l lista de llaves incluida de mi amigo
-$ echo "este mensaje es secreto" > doc_no_cifrado.txt # mensaje para nuestro compa 
-cat doc-no_cifrado.txt # verificamos la creacion del contenido 
-gpg --output doc_cifrado.txt --encrypt --recipient xxxxxxxxxxxxxxxxxxxxxxx doc_no_cifrado.txt # cifrar el documento 
-gpg --decrypt cifrado_fran.txt # desifra el documento  
-gpg --clearsign # firma el texto 
-gpg --sign #firma en formato binario
-gpg --detach-sign #crea una firma separada
-gpg --verify # verifica la firma recibida 
-gpg --edit-key   Trust #Asigna una confianza a la llave de su pareja
-gpg --sign-key #firma la llave de su pareja
-gpg --encrypt --sign #cifra y firma en un solo comando 
-gpg --decrypt # descifra y valida la firma en un solo paso 
-cat # comprueba el contenido del archivo 
+gpg --list-secret-keys --keyid-format=long  # command to list private keys
+gpg --armor --export-secret-keys xxxxx # exports the secret key
+gpg --import MYPARTNER_public_key.asc # imports a friend's public key
+gpg --list-keys # lists all keys including my friend's
+$ echo "this message is secret" > unencrypted_doc.txt # message for our partner
+cat unencrypted_doc.txt # verify the content was created
+gpg --output encrypted_doc.txt --encrypt --recipient xxxxxxxxxxxxxxxxxxxxxxx unencrypted_doc.txt # encrypt the document
+gpg --decrypt fran_encrypted.txt # decrypt the document
+gpg --clearsign # sign in plain text
+gpg --sign # sign in binary format
+gpg --detach-sign # create a detached signature
+gpg --verify # verify the received signature
+gpg --edit-key Trust # assign a trust level to your partner's key
+gpg --sign-key # sign your partner's key
+gpg --encrypt --sign # encrypt and sign in a single command
+gpg --decrypt # decrypt and validate the signature in one step
+cat # check the content of the file
