28365439be2b922338806bd92aabdc5a83243dcb
Cris10 <cristopher0066ok@gmail.com>
2026-03-30 15:51:01 +0000

    testtxt

--- fc_script.sh
+++ fc_script.sh
@@ -4,3 +4,5 @@ sudo apt install parted #Open the terminal and make sure the package manager is 
 pip install numpy #Install numpy 
 sudo parted -l && echo -e "\n--\n"&& lsblk -f && echo -e "\n---\n" # List all disks and their partitions and display the storage devices in a tree structure.
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if the system is using UEFI.
+echo "mi archivo" > test.tx
+stat test.txt
