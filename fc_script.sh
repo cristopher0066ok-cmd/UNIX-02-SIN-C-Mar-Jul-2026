06850de27a6d8426e16e8f468b132db45cb3a45c
Cris10 <cristopher0066ok@gmail.com>
2026-03-30 15:36:37 +0000

    commit_bios

---
+++ fc_script.sh
@@ -0,0 +1,6 @@
+sudo apt update #  Install Available Updates 
+sudo apt upgrade # refreshes the local package index 
+sudo apt install parted #Open the terminal and make sure the package manager is up to date.
+pip install numpy #Install numpy 
+sudo parted -l && echo -e "\n--\n"&& lsblk -f && echo -e "\n---\n" # List all disks and their partitions and display the storage devices in a tree structure.
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #Check if the system is using UEFI.
