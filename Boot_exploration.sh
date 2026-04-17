1a26756c820c3b694e4786264a299cce59f6df7d
Cris10 <cristopher0066ok@gmail.com>
2026-04-17 14:45:30 +0000

    git-clone

--- Boot_exploration.sh
+++ Boot_exploration.sh
@@ -4,4 +4,8 @@
 #mkdir -- -rf → creates a directory named “-rf”
 #rmdir -- -rf → attempts to remove a directory named “-rf” (only if it is empty)
 #ls --help → shows a short, quick help summary of the ls command (fits on one screen).
-#man ls → opens the full manual in English with detailed information about the ls command
+#mann ls → opens the full manual in English with detailed information about the ls command
+#man git-clone man git-clone = shows the complete manual for git clone with full details.
+#--depth <depth>
+#Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the
+#histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
