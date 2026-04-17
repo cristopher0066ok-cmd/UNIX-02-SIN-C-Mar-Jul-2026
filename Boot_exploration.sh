a5af1005e10be5b91bc2f9e18ebfe4e83bf1a49f
Cris10 <cristopher0066ok@gmail.com>
2026-04-17 14:30:41 +0000

    .

--- Boot_exploration.sh
+++ Boot_exploration.sh
@@ -1,1 +1,3 @@
-#ls -a is a system command used in the terminal to list files, including hidden ons
+#ls -a is a system command used in the terminal to list files, including hidden ones
+#In Linux, ls -l -ah is a combination of options for the ls command used to list files with more detailed information.”
+#ls -lah  view everything with detailed information and human-readable file sizes.”
