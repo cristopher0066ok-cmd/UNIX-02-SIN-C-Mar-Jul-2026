e0761d66300e828f6190213bd6d88034565e0904
Cris10 <cristopher0066ok@gmail.com>
2026-04-17 14:34:41 +0000

    mkdir

--- Boot_exploration.sh
+++ Boot_exploration.sh
@@ -1,1 +1,5 @@
-#ls -a is a system command used in the terminal to list files, including hidden ons
+#ls -a is a system command used in the terminal to list files, including hidden ones
+#In Linux, ls -l -ah is a combination of options for the ls command used to list files with more detailed information.”
+#ls -lah  view everything with detailed information and human-readable file sizes.”
+#mkdir -- -rf → creates a directory named “-rf”
+#rmdir -- -rf → attempts to remove a directory named “-rf” (only if it is empty)
