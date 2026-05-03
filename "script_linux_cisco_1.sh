1a3feea77bfc07260dd52ab95385d25dbc9caef6
Cris10 <cristopher0066ok@gmail.com>
2026-05-03 03:57:46 +0000

    commit

---
+++ "script_linux_cisco_1.sh
@@ -0,0 +1,24 @@
+ls:
+#List names.
+ls -l: 
+#List with details (size, date, permissions).
+ls -r: 
+#Reverse the order.
+ls -t: 
+#Sort by time (newest first).
+ls -S: 
+#Sort by size (largest first).
+pwd
+#"Where am I?" (shows current path).
+cd:
+#Go to Home.
+cd ..:
+#Go back/up one folder.
+cd /: 
+#Go to the system root.
+cd ~:
+#Go to user folder.
+su -: 
+#Switch to Superuser (Admin/root).
+exit:
+#Close session or return to previous user.
