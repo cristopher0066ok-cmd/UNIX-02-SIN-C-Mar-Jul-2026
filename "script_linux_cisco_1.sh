2506cc308fe3db468c94c048ec375037d9578a1d
Cris10 <cristopher0066ok@gmail.com>
2026-05-03 04:20:33 +0000

    chmod

--- "script_linux_cisco_1.sh
+++ "script_linux_cisco_1.sh
@@ -22,3 +22,29 @@ su -:
 #Switch to Superuser (Admin/root).
 exit:
 #Close session or return to previous user.
+sl 
+#We have configured the steam locomotive, sl command to require administrative access.
+sudo sl
+#Runs the "Steam Locomotive" animation with admin privileges.
+cd ~/Documents
+#Moves you to the current user's Documents folder.
+ls -l hello.sh 
+#Shows the specific details and permissions for that file
+./hello.sh  
+#Runs/executes the script located in the current folder.
+chmod u+x hello.sh
+#Grants execute permission to the owner
+sudo chown root hello.sh 
+#Changes the file owner to "root"
+cat alpha.txt   
+#Displays the entire content of the file.
+head alpha.txt 
+#isplays the beginning of the file first 10 lines
+tail alpha.txt 
+#Displays the end of the file last 10 lines
+-n 5: 
+#Flag used to specify a different number of lines
+cp /etc/passwd .
+#Copies the system's password file to your current location
+dd 
+#Data Duplicator" used for low-level copying
