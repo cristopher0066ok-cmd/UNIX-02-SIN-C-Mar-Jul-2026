3d4b4c7eb569d9f36c9d408b7b41896f00ccbaaf
Cris10 <cristopher0066ok@gmail.com>
2026-05-04 04:21:32 +0000

    stat.

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,16 @@ ls -lai
 #l=list a=all i=inode
 ls -l -a -i
 #the same options but in a separate way
+stat .
+#File: .
+#Size: 4096            Blocks: 8          IO Block: 4096   directory
+#Device: 7,4     Inode: 1578789     Links: 2
+#Access: (0777/drwxrwxrwx)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+#Access: 2026-05-04 04:16:44.331494113 +0000
+#Modify: 2026-05-04 04:16:42.249494253 +0000
+#Change: 2026-05-04 04:16:42.249494253 +0000
+#Birth: 2026-05-04 04:16:42.249494253 +0000
+#stat . gives you the "ID card" of your current folder:
+#Inode (1578789): The unique internal ID number of the folder in the system.
+#Links (2): The number of connections to the folder (itself and the . shortcut inside).
+#Device (7,4): The specific disk partition where the folder is saved.
