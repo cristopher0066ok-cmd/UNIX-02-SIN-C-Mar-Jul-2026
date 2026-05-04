79635371dcb70e6121d0e9dcffb4f18abd3e6d09
Cris10 <cristopher0066ok@gmail.com>
2026-05-04 04:13:22 +0000

    commit

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+cd calculadora
+#relative route
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
+#absoulute route
+ls -lai
+#l=list a=all i=inode
+ls -l -a -i
+#the same options but in a separate way
