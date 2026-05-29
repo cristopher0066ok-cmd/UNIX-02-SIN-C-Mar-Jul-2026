099b24623a2a1ab00e3c39c0131af134e6276a36
Cris10 <cristopher0066ok@gmail.com>
2026-05-29 15:41:45 +0000

    commit3

--- security2.zsh
+++ security2.zsh
@@ -38,3 +38,10 @@ echo $$
 touch ~/after_newgrp.txt
 # Lists the detailed attributes of the newly created file (permissions, owner, group, date)
 ls -la ~/after_newgrp.txt
+# Create a file inside the subshell
+touch ~/dentro_de_newgrp.txt
+ls -la ~/dentro_de_newgrp.txt
+# The group is now 'desarrolladores'
+# Create a directory
+mkdir -p ~/proyecto_dev/src
+ls -la ~/
