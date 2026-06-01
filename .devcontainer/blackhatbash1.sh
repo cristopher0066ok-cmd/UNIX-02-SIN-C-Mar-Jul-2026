67b26a00bd923cfeb8fdfe77f60572f74ab4fca5
Cris10 <cristopher0066ok@gmail.com>
2026-06-01 15:26:44 +0000

    commit6

--- .devcontainer/blackhatbash1.sh
+++ .devcontainer/blackhatbash1.sh
@@ -1,20 +1,14 @@
+#!/usr/bin/env bash
 set -x
-$ bash --version
+
+bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
-ps -e -f 
+ps -e -f
 ps -ef
 df --human-readable
-!/usr/bin/env bash
-#bash -r blackhatbash1.sh
-#bash -x blackhatbash1.sh
-#When we execute these commands, we enter a loop
-script.sh 
-#bash script.sh
- cd ~ 
- chmod u+x blackhatbash1.sh
- set +x
 
+set +x
 
