0ce15e2d16951587981444370582bc925da5ae3b
Cris10 <cristopher0066ok@gmail.com>
2026-06-05 15:09:08 +0000

    commit

--- array.sh
+++ array.sh
@@ -4,6 +4,8 @@ DIRECCIONES_IP=(192.168.1.1 192.168.1.2 192.168.1.3)
 unset IP_ADDRESSES[1]
 DIRECCIONES_IP[0]=" 192.168.1.10 "
 # Imprime todos los elementos del array
+set -x
 echo "${DIRECCIONES_IP[*]}"
+set +x
 # Imprime solo el primer elemento del array
 echo "${DIRECCIONES_IP[0]}"
