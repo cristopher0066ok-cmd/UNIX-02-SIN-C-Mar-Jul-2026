e23847002a8c448e949985c5ff33d3252bac299b
Cris10 <cristopher0066ok@gmail.com>
2026-06-05 15:43:44 +0000

    commit16

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -11,3 +11,22 @@ ls; ps; whoami
  cat output.txt
  Hello World!
  Goodbye!
+
+ls -l / &> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.tx
+
+lzl 2> error.txt
+error de gato.txt
+cat < output.txt
+
+cat << EOF
+> Black Hat Bash
+> by No Starch Press
+> EOF
+
+Black Hat Bash
+by No Starch Press
