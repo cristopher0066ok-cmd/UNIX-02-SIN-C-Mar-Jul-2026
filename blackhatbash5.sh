c99f7b0ba4b87e3c9435317baa714d483abb568d
Cris10 <cristopher0066ok@gmail.com>
2026-06-05 15:31:10 +0000

    commit15

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,11 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 "lzl || echo 'el comando lzl falló'"
+#------------------------------------
+ echo "Hello World!" > output.txt
+ cat output.txt
+ Hello World!
+ echo "Goodbye!" >> output.txt 
+ cat output.txt
+ Hello World!
+ Goodbye!
