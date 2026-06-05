13a9b68ad35cd660fa49d1a26ebdd47d661a13d1
Cris10 <cristopher0066ok@gmail.com>
2026-06-05 15:15:07 +0000

    commit13

---
+++ background..sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script will send the sleep command to the background.
+echo "Sleeping for 60 seconds..."
+sleep 60 &
+
+# Creates a file
+echo "Creating the file test123"
+touch test123
+
+# Deletes a file
+echo "Deleting the file test123"
+rm test123
