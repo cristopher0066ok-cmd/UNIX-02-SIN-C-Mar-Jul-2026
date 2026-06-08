0609eba0e4367d5778a4a0841a69acb98ad52840
Cris10 <cristopher0066ok@gmail.com>
2026-06-08 15:25:11 +0000

    commit21

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -4,7 +4,11 @@ SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping -c 4 "${TARGET}"
+ping -c 3 "${TARGET}"
 
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
+
+for args in "$@"; do
+echo "${args}"
+done
