bff71e0f270c1115a6db0b735f6699fe4e152286
Cris10 <cristopher0066ok@gmail.com>
2026-06-12 14:32:41 +0000

    commit32

--- if_elif.sh
+++ if_elif.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 USER_INPUT="${1}"
 
-# ❶ Verifica si el argumento está vacío
+# ❶ Check if the argument is empty
 if [[ -z "${USER_INPUT}" ]]; then
     echo "You must provide an argument!"
     exit 1
 fi
 
-# ❷ Verifica si es un archivo, ❸ un directorio, o ❹ ninguno
+# ❷ Check if it is a file, ❸ a directory, or ❹ neither
 if [[ -f "${USER_INPUT}" ]]; then
     echo "${USER_INPUT} is a file."
 elif [[ -d "${USER_INPUT}" ]]; then
     echo "${USER_INPUT} is a directory."
 else
     echo "${USER_INPUT} is not a file or a directory."
-    # Opcional: puedes cambiar este echo por lo que necesites en ❹
-fi 
+    # Optional: you can change this echo for whatever you need in ❹
+fi
